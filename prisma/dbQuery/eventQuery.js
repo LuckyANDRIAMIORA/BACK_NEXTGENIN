d0bb658ae4311e7c1037137e8f43328276010323
malantoniaina <livanorosoa@gmail.com>
2024-03-07 11:00:04 +0100

    date

--- prisma/dbQuery/eventQuery.js
+++ prisma/dbQuery/eventQuery.js
@@ -17,6 +17,7 @@ let createEvent = async (event) => { //et ca aussi est une fonction promise
           data:{
             eventname : event.eventname,
             eventdescription : event.eventdescription,
+            eventdate : event.eventdate,
             club : {
                 connect : { id : event.clubId}
             }
@@ -28,4 +29,31 @@ let createEvent = async (event) => { //et ca aussi est une fonction promise
     }
 }
 
-module.exports={getEvents,createEvent}
+let deleteEvent = async(res, id) => {
+    try {
+      const existingEvent = await prisma.Event.findUnique({
+        where: {
+          id: id,
+        },
+      })
+  
+      if (!existingEvent) {
+        res.status(500).json({message: "Event not found" })
+        res.end()
+      }
+  
+      await prisma.Event.delete({
+        where: {
+          id: id,
+        },
+      })
+      prisma.$disconnect()
+      res.status(200).json({message: "Event deleted" })
+      res.end()
+    } catch (error) {
+      prisma.$disconnect()                  
+      throw new Error(error.message)
+    }
+  }
+
+module.exports={getEvents,createEvent,deleteEvent}
